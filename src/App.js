9150abefa9f959b20160d9cea18507c2cb4bc591
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-04-22 04:49:54 -0700

    display correct entries on page selection by slicing data from allKittens. I did it.

--- src/App.js
+++ src/App.js
@@ -30,9 +30,24 @@ class App extends Component {
 //call loadData method and pass in baseUrl from props with page set to 1
   componentWillMount() {
     this.loadData(`${this.props.baseUrl}/1`);
-    this.setState({
-      kittens: allKittens
-    });
+    this.loadData(`${this.props.baseUrl}/2`);
+    this.loadData(`${this.props.baseUrl}/4`);
+    this.loadData(`${this.props.baseUrl}/7`);
+    this.loadData(`${this.props.baseUrl}/8`);
+    this.loadData(`${this.props.baseUrl}/4/6`);
+    this.loadData(`${this.props.baseUrl}/4/7`);
+    this.loadData(`${this.props.baseUrl}/4/8`);
+    this.loadData(`${this.props.baseUrl}/4/9`);
+    this.loadData(`${this.props.baseUrl}/7/6`);
+    this.loadData(`${this.props.baseUrl}/7/7`);
+    this.loadData(`${this.props.baseUrl}/4/8`);
+    this.loadData(`${this.props.baseUrl}/8/7`);
+    this.loadData(`${this.props.baseUrl}/8/8`);
+    this.loadData(`${this.props.baseUrl}/89/1`);
+    this.loadData(`${this.props.baseUrl}/11/8`);
+    this.loadData(`${this.props.baseUrl}/11/9`);
+    this.loadData(`${this.props.baseUrl}/13/7`);
+    this.loadData(`${this.props.baseUrl}/13/8`);
   }
 
   render() {
@@ -42,17 +57,18 @@ class App extends Component {
         <Navbar brand='Kitten Pages' left >
         </Navbar>
 
-      {/*Column to display list of all kittens*/}
-        <Col s={8} m={10} className="s2 m1">
-          <KittenList listOfKittens={this.state.kittens} />
-        </Col>
-
         <Col s={12}>
           <Pagination
             items={this.state.totalPages}
             activePage={this.state.activePage}
             onSelect={this.handlePaginationSelect}/>
         </Col>
+
+      {/*Column to display list of all kittens*/}
+        <Col s={8} m={10} className="s2 m1">
+          <KittenList listOfKittens={this.state.kittens} />
+        </Col>
+
       </div>
     );
   }
@@ -73,9 +89,19 @@ class App extends Component {
            allKittens.push(item);
            this.sortKittens();
          }
+
        });
        //update state in order to render kittens
-         this.dataForPage();
+        // if(this.state.page !== 3 && this.state.page !== 5 && this.state.page !== 6 && this.state.page !== 9 && this.state.page !== 10){
+        //   this.setState({
+        //     kittens: json
+        //   });
+        // } else {
+          this.setState({
+            kittens: allKittens
+          });
+        // }
+
 
       }).catch(error => {
         console.log(error);
@@ -89,67 +115,114 @@ class App extends Component {
     });
   }
 
-  dataForPage() {
-    if(this.state.page === 1) {
-      this.setState({
-        kittens: allKittens.splice(0,10)
-      })
-    } else if(this.state.page === 2) {
-      this.setState({
-        kittens: allKittens.splice(10, 21)
-      })
-    } else if(this.state.page === 3) {
-      this.setState({
-        kittens: allKittens.splice(21, 31)
-      })
-    } else if(this.state.page === 5) {
-      this.setState({
-        kittens: allKittens.splice(31, 40)
-      })
-    } else if(this.state.page === 6) {
-      this.setState({
-        kittens: allKittens.splice(40, 51)
-      })
-    }
-    else if(this.state.page === 9) {
-      this.setState({
-        kittens: allKittens.splice(51, 61)
-      })
-    } else if(this.state.page === 10) {
-      this.setState({
-        kittens: allKittens.splice(61, 71)
-      })
-    }
-  }
+  // dataForPage() {
+  //   if(this.state.page === 1) {
+  //     this.setState({
+  //       kittens: allKittens.splice(0,10)
+  //     })
+  //   } else if(this.state.page === 2) {
+  //     this.setState({
+  //       kittens: allKittens.splice(10, 21)
+  //     })
+  //   } else if(this.state.page === 3) {
+  //     this.setState({
+  //       kittens: allKittens.splice(21, 31)
+  //     })
+  //   } else if(this.state.page === 5) {
+  //     this.setState({
+  //       kittens: allKittens.splice(31, 40)
+  //     })
+  //   } else if(this.state.page === 6) {
+  //     this.setState({
+  //       kittens: allKittens.splice(40, 51)
+  //     })
+  //   }
+  //   else if(this.state.page === 9) {
+  //     this.setState({
+  //       kittens: allKittens.splice(51, 61)
+  //     })
+  //   } else if(this.state.page === 10) {
+  //     this.setState({
+  //       kittens: allKittens.splice(61, 71)
+  //     })
+  //   }
+  // }
 
 //call loadData on page selection
-  handlePaginationSelect(selectedPage) {
-    let page = selectedPage;
-  //if not a restricted page call data by page
-    if(page !== 3 && page !== 5 && page !== 6 && page !== 9 && page !== 10) {
-     this.loadData(`${this.props.baseUrl}/${page}`);
-  //if restricted page, manually fetch missing items from non-restricted pages
-   } else if(page === 3) {
-      this.loadData(`${this.props.baseUrl}/4/6`);
-      this.loadData(`${this.props.baseUrl}/4/7`);
-      this.loadData(`${this.props.baseUrl}/4/8`);
-    } else if(page === 5) {
-      this.loadData(`${this.props.baseUrl}/4/9`);
-      this.loadData(`${this.props.baseUrl}/7/6`);
-      this.loadData(`${this.props.baseUrl}/7/7`);
-      this.loadData(`${this.props.baseUrl}/4/8`);
-    } else if(page === 6) {
-      this.loadData(`${this.props.baseUrl}/8/7`);
-      this.loadData(`${this.props.baseUrl}/8/8`);
+handlePaginationSelect(selectedPage) {
+  let page = selectedPage;
+  this.sortKittens();
+//if not a restricted page call data by page
+  if(page === 1) {
+    this.setState({
+      kittens: allKittens.slice(0, 10)
+    })
+ } else if(page === 2) {
+   this.setState({
+     kittens: allKittens.slice(10, 20)
+   })
+   console.log(allKittens.slice(10, 20));
+  } else if(page === 3) {
+   this.setState({
+     kittens: allKittens.slice(20, 30)
+   })
+ } else if(page === 4) {
+    this.setState({
+      kittens: allKittens.slice(30, 40)
+    })
+   } else if(page === 5) {
+     this.setState({
+       kittens: allKittens.slice(40, 50)
+     })
+  } else if(page === 6) {
+    this.setState({
+      kittens: allKittens.slice(50, 60)
+    })
+  } else if(page === 7) {
+    this.setState({
+      kittens: allKittens.slice(60, 70)
+    })
+  } else if(page === 8) {
+     this.setState({
+       kittens: allKittens.slice(70, 80)
+     })
     } else if(page === 9) {
-      this.loadData(`${this.props.baseUrl}/89/1`);
-      this.loadData(`${this.props.baseUrl}/11/8`);
-      this.loadData(`${this.props.baseUrl}/11/9`);
-    } else if(page === 10) {
-      this.loadData(`${this.props.baseUrl}/13/7`);
-      this.loadData(`${this.props.baseUrl}/13/8`);
-    }
+      this.setState({
+        kittens: allKittens.slice(80, 90)
+      })
+  } else if(page === 10) {
+    this.setState({
+      kittens: allKittens.slice(90, 100)
+    })
   }
+}
+  // handlePaginationSelect(selectedPage) {
+  //   let page = selectedPage;
+  // //if not a restricted page call data by page
+  //   if(page !== 3 && page !== 5 && page !== 6 && page !== 9 && page !== 10) {
+  //    this.loadData(`${this.props.baseUrl}/${page}`);
+  // //if restricted page, manually fetch missing items from non-restricted pages
+  //  } else if(page === 3) {
+  //     this.loadData(`${this.props.baseUrl}/4/6`);
+  //     this.loadData(`${this.props.baseUrl}/4/7`);
+  //     this.loadData(`${this.props.baseUrl}/4/8`);
+  //   } else if(page === 5) {
+  //     this.loadData(`${this.props.baseUrl}/4/9`);
+  //     this.loadData(`${this.props.baseUrl}/7/6`);
+  //     this.loadData(`${this.props.baseUrl}/7/7`);
+  //     this.loadData(`${this.props.baseUrl}/4/8`);
+  //   } else if(page === 6) {
+  //     this.loadData(`${this.props.baseUrl}/8/7`);
+  //     this.loadData(`${this.props.baseUrl}/8/8`);
+  //   } else if(page === 9) {
+  //     this.loadData(`${this.props.baseUrl}/89/1`);
+  //     this.loadData(`${this.props.baseUrl}/11/8`);
+  //     this.loadData(`${this.props.baseUrl}/11/9`);
+  //   } else if(page === 10) {
+  //     this.loadData(`${this.props.baseUrl}/13/7`);
+  //     this.loadData(`${this.props.baseUrl}/13/8`);
+  //   }
+  // }
 }
 
 export default App;
