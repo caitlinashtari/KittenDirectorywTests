a59c4ae626a9f7bc1feac1e2eedc46f20ca4b7bb
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-04-22 15:33:05 -0700

    add 4th passsing test to see if response body contains Name.

--- spec/api/APItests_spec.js
+++ spec/api/APItests_spec.js
@@ -1,9 +1,35 @@
 var frisby = require('frisby');
+
 frisby.create('Get Zapproved API')
   .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .toss();
+
+// frisby.create('Ensure response has proper JSON types in specified keys')
+//   .expectJSONTypes('args', [{
+//     'Name': String,
+//     'Location': String,
+//     'Number': Number
+//   }])
+//   .toss()
+
+frisby.create('Ensure this is a genuine response from API')
+  .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1')
+  .expectStatus(200)
+  .expectBodyContains('Name')
+  .toss()
+
+  frisby.create('Ensure test has name, location, and number')
+    .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1/1')
+    .expectJSON([
+      {
+        'Name': "Chester Soderblom",
+        'Location': "Simmesport",
+        'Number': 1
+      }
+    ])
+  .toss()
 
   frisby.create('Ensure test has name, location, and number')
     .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1/1')
