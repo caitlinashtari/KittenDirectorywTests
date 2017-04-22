cb90186ce6c7f89a623c4060e32c42b810cb2237
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-04-22 15:26:04 -0700

    add a second passing test to test if request for page 1 item 1 equals page 1 item 1 values.

--- spec/api/APItests_spec.js
+++ spec/api/APItests_spec.js
@@ -4,3 +4,14 @@ frisby.create('Get Zapproved API')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .toss();
+
+  frisby.create('Ensure test has name, location, and number')
+    .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1/1')
+    .expectJSON([
+      {
+        'Name': "Chester Soderblom",
+        'Location': "Simmesport",
+        'Number': 1
+      }
+    ])
+  .toss()
