25f165009e57196ad276146a1487179e9f2538bf
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-04-22 17:05:58 -0700

    wrap up my exploration of testing API with Frisby.

--- spec/api/APItests_spec.js
+++ spec/api/APItests_spec.js
@@ -6,18 +6,19 @@ frisby.create('Get Zapproved API')
   .expectHeaderContains('content-type', 'application/json')
   .toss();
 
-// frisby.create('Ensure response has proper JSON types in specified keys')
-//   .expectJSONTypes('args', [{
-//     'Name': String,
-//     'Location': String,
-//     'Number': Number
-//   }])
-//   .toss()
-
 frisby.create('Ensure this is a genuine response from API')
   .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1')
   .expectStatus(200)
   .expectBodyContains('Name')
+  .toss()
+
+frisby.create('Ensure response has proper JSON types in specified keys')
+  .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1')
+  .expectJSONTypes([{
+    'Name': String,
+    'Location': String,
+    'Number': Number
+  }])
   .toss()
 
 frisby.create('Ensure test has name, location, and number')
@@ -31,36 +32,28 @@ frisby.create('Ensure test has name, location, and number')
   ])
   .toss()
 
-frisby.create('Ensure test has name, location, and number')
-    .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1/1')
-    .expectJSON([
-      {
-        'Name': "Chester Soderblom",
-        'Location': "Simmesport",
-        'Number': 1
-      }
-    ])
-    .toss()
-
 frisby.create('Ensure page 3 returns 400 error response')
   .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/3')
   .expectStatus(400)
   .toss()
 
-
-frisby.create('Inspection of the JSON HTTP response')
-  .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1')
-  .inspectJSON()
-  .toss()
-
-frisby.create('Just a quick inspection of the HTTP headers response')
-  .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/1')
-  .inspectHeaders()
-  .toss()
-
-//itterate over all possible pages and numbers of items
+// itterate over all possible pages at default of 10 items/page
 for(var i = 1; i <= 100; i++) {
-frisby.create('Just a quick inspection of the HTTP status code response')
-    .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/' + i).inspectStatus()
-    .toss()
-  }
+  frisby.create('Inspect HTTP status code response for pages 1-100')
+      .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/' + i)
+      .inspectBody()
+      .toss()
+}
+
+//Attempt to test all items up to ten for all pages up to 100 ENDLESS LOOP... i think.
+// for(var i = 1; i <= 10; i++) {
+//   var j = 1;
+//   while(j <= 100) {
+//     frisby.create('Inspection of the JSON HTTP response for number of items on each page')
+//       .get('https://50iyt60g2b.execute-api.us-west-2.amazonaws.com/prod/load/' + j + i)
+//       .inspectBody()
+//       .toss()
+//       j++
+//   }
+// }
+//
